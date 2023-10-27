cb3c9f96d6a43c600e1592a3283cf77cf649ca7a
Inna <prizrak1609@gmail.com>
2023-10-27 19:08:01 +0300

    Add project files.

---
+++ Play.cs
@@ -0,0 +1,34 @@
+﻿//Завдання 3: Додайте до першого завдання реалізацію інтерфейсу IDisposable.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NET_Homework_7_3
+{
+    internal class Play: IDisposable
+    {
+        public string NamePlay;
+        public string FullNameAuthor;
+        public string Genre;
+        public string Year;
+        public Play(string N, string A, string G, string Y)
+        {
+            NamePlay = N;
+            FullNameAuthor = A;
+            Genre = G;
+            Year = Y;
+        }
+        ~Play()
+        {
+            Console.WriteLine("\nВідпрацювання фіналізатора.");
+            Thread.Sleep(600000);//Затримка часу спрацювання фіналізатора
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("\n\nВідпрацювання метода Dispose()");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Завдання 3: Додайте до першого завдання реалізацію інтерфейсу IDisposable.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_Homework_7_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Play play = new Play("Назва п'єси", "Автор п'єси", "Жанр п'єси", "Рік написання п'єси");
+            Console.WriteLine($"П'єса: {play.NamePlay} {play.FullNameAuthor} {play.Genre} {play.Year}");
+
+            //play.Dispose();//безпосередньо виклик методу Dispose() через об'єкт
+
+            using (play)
+            {
+                Console.WriteLine("\n\nВідпрацювання конструкції using");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
